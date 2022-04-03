690b9395213f161f96e3c808f48d98c91f665b9b
Arash Sa <101516570+Arashsa31@users.noreply.github.com>
2022-04-02 22:07:15 -0400

    Add files via upload

---
+++ C2F.py
@@ -0,0 +1,18 @@
+# asks for the user input
+print("Please enter the current temperature in Celsius: " , end="")
+
+# takes the user input and places it into the cTemp variable 
+cTemp = float(input())
+
+# repeats what the user entered 	    
+print("You entered the current temperature as",cTemp, "degrees in Celsius.")
+
+# use the conversion formula to convert Fahrenheit temperature to Celsius temperature
+fTemp = 9/5 * cTemp + 32;
+
+# prints the converted degree to the user
+print("Your converted temperature of", cTemp, "Celsius in Fahrenheit is ", end="")
+
+# rounds the final tempature
+txt = "{:.3f}"
+print(txt.format(fTemp))
